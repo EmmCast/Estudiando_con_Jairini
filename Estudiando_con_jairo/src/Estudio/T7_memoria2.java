247698dc28d1e5034f04aba18845ab1eadceb2eb
EmmCast <67034706+EmmCast@users.noreply.github.com>
2020-07-15 19:48:51 -0500

    15/07/20

---
+++ Estudiando_con_jairo/src/Estudio/T7_memoria2.java
@@ -0,0 +1,21 @@
+/*Elaborar un algoritmo que permita calcular el numero de CDs necesarios para hacer una copia de seguridad, de la información almacenada en un 
+ * disco cuya capacidad se conoce, considerar que el disco duro esta lleno de información además expresando en gb y un cd virgen tiene 700 mb 
+ * de capacidad y un gb es igual a 1024 mb
+ */
+package Estudio;
+public class T7_memoria2 {
+	public static void main (String arg[]) {
+		StringBuffer impt=new StringBuffer();
+		double R=ND(2.0);
+		impt.append("El numero de discos que se necesitan para " )
+		.append(R);
+		System.out.println(impt.toString());
+		
+	}
+
+	private static double ND(double G) {
+		double res=0;
+		res=Math.ceil((G*1024)/700);
+		return res;
+	}
+}
